b040d14dec0bae00931f8c866a93acd3cab4bba0
Jonatan Timoteo <jonatan_ts@hotmail.com>
2022-04-29 10:47:57 -0300

    header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
+import logo from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+
 export function Header() {
   return(
-    <header>
-      Oi mamae
-    </header>
+    <Container>
+      <Content>
+        <img src={logo} alt="Jt money" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
   )
 }
