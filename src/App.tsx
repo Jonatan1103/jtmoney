e15874025c1fc8572370f7833fa93062371a9b48
Jonatan Timoteo <jonatan_ts@hotmail.com>
2022-05-02 12:06:44 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { GlobalStyle } from "./styles/global"
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
+import { useState } from "react";
 
 export function App() {
+  const [isNewTransectionModalOpen, setIsNewTransectionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransectionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransectionModalOpen(false)
+  }
+  
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransectionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+
+      <Modal 
+          isOpen={isNewTransectionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cadastrar transações</h2>
+      </Modal>
+
       <GlobalStyle/>
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logo from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
+interface HeaderProps {
+  onOpenNewTransectionModal : () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransectionModal } : HeaderProps) {
+
   return(
     <Container>
       <Content>
         <img src={logo} alt="Jt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransectionModal}>
           Nova transação
         </button>
       </Content>
